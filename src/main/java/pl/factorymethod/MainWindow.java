0ded397444eabb0861f03ebc4f29f8cff739997a
Adrian <adrian.kozlowski@outlook.com>
2018-07-13 20:48:19 +0200

    dimensions

--- src/main/java/pl/factorymethod/MainWindow.java
+++ src/main/java/pl/factorymethod/MainWindow.java
@@ -1,14 +1,9 @@
 package pl.factorymethod;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebEngine;
@@ -16,16 +11,8 @@ import javafx.scene.web.WebEvent;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import javax.swing.plaf.nimbus.State;
 import java.io.IOException;
-import java.net.CookieHandler;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.security.GeneralSecurityException;
 import java.util.ResourceBundle;
 
 
@@ -33,6 +20,7 @@ public class MainWindow extends Application implements Initializable {
 
     @FXML
     public WebView webView;
+    BorderPane root;
 
     public static void main(String[] args) {
         launch(args);
@@ -45,8 +33,9 @@ public class MainWindow extends Application implements Initializable {
 
     @Override
     public void start(Stage stage) throws IOException {
-        BorderPane root = FXMLLoader.load(getClass().getClassLoader().getResource("MainWindow.fxml"));
-        Scene scene = new Scene(root, 300, 275);
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("MainWindow.fxml"));
+
+        Scene scene = new Scene(root, 700, 875);
         stage.setTitle("FXML Welcome");
         stage.setScene(scene);
         stage.show();
@@ -66,8 +55,7 @@ public class MainWindow extends Application implements Initializable {
             System.out.println("Alert Event  -  Message:  " + wEvent.getData());
         });
 
-        java.net.CookieManager manager = new java.net.CookieManager();
-        CookieHandler.setDefault(manager);
+        engine.setUserAgent("FactoryMethod web browser");
 
         engine.load("https://messenger.com/");
     }
